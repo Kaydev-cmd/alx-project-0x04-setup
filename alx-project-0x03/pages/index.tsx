92c944fa180e50f18e4f70e827bd7ef641a8ddd1
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-05 14:10:03 +0200

    feat: implement imperative routing function using useRouter and PageRouteProps

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -6,12 +6,17 @@ interface PageRouteProps {
   pageRoute: string;
 }
 
-const Home = () => {
+export default function Home() {
+  const router = useRouter();
+
+  // Imperative routing with useRouter
+  const routeToNextPage = ({ pageRoute }: PageRouteProps) => {
+    router.push(pageRoute, undefined, { shallow: false });
+  };
+  
   return (
     <div>
       <h1>Welcome to Splash App</h1>
     </div>
   );
-};
-
-export default Home;
+}
