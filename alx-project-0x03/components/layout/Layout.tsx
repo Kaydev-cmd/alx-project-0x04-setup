6efd29218407d622d570d2e630a6de27ec232527
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-05 11:24:38 +0200

    feat: define LayoutProps interface with children of type ReactNode

--- alx-project-0x03/components/layout/Layout.tsx
+++ alx-project-0x03/components/layout/Layout.tsx
@@ -3,6 +3,10 @@ import { ReactNode } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 
+interface LayoutProps {
+  children: ReactNode;
+}
+
 const Layout: React.FC = () => {
   return <div>Layout</div>;
 };
