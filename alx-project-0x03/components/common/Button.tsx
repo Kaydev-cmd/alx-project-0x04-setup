8350d05916cef248bbde39cb62d5cb6614e0d927
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-05 10:59:07 +0200

    feat: add customizable Button component with dynamic color, size, and action support

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -21,4 +21,15 @@ const Button = ({
         green: "bg-green-500",
       }[buttonBackgroundColor]
     : "bg-slate-500";
+
+  return (
+    <button
+      onClick={action}
+      className={`${backgroundColorClass} ${buttonSize} px-6 py-2 text-sm font-semibold rounded-lg hover:${backgroundColorClass}/50 transition duration-300 text-white`}
+    >
+      {buttonLabel}
+    </button>
+  );
 };
+
+export default Button;
