c75f71a9fa74546669aadffa330213678a8b7508
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-05 11:01:08 +0200

    feat: set up Header component with basic structure and imported Button and Link

---
+++ alx-project-0x03/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Link from "next/link";
+import Button from "../common/Button";
+
+const Header: React.FC = () => {
+  return <div>Header</div>;
+};
+
+export default Header;
