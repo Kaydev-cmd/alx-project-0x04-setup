178345af8aa77328c01756751aaaeb0a6d5641e6
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-05 15:55:59 +0200

    refactor: import LayoutProps interface from interfaces

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import { ReactNode } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-
-interface LayoutProps {
-  children: ReactNode;
-}
+import { LayoutProps } from "@/interfaces";
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
