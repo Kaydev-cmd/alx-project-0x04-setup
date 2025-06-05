683f76def8041957b5e2727b9a120ca373d71155
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-05 13:58:04 +0200

    refactor: change function ssyntax to arrow function

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 
-export default function Home() {
+const Home = () => {
   return <h1>Welcome to Splash App</h1>;
-}
+};
+
+export default Home;
