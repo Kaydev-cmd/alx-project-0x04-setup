2e7936542698a354147aff0c81f0b01c525d6fd2
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-05 16:39:00 +0200

    feat: add styled 404 page with gradient background and error message

--- alx-project-0x03/pages/404.tsx
+++ alx-project-0x03/pages/404.tsx
@@ -3,7 +3,15 @@ import Link from "next/link";
 import { FaHome } from "react-icons/fa";
 
 const Custom404: React.FC = () => {
-  return <div>404</div>;
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-purple-500 to-pink-500 flex flex-col justify-center items-center text-white">
+      {/* Main Heading */}
+      <h1 className="text-6xl font-bold mb-4">Oops! 😱</h1>
+      <p className="text-2xl mb-8">
+        We can't seem to find the page you're looking for.
+      </p>
+    </div>
+  );
 };
 
 export default Custom404;
