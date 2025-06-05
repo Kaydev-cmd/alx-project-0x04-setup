d9b0ddc2a644bf453e11d12288d1bd5ca3799955
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-05 13:54:11 +0200

    refactor: change welcome message to 'Welcome to Splash App'

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -2,5 +2,5 @@ import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 
 export default function Home() {
-  return <h1>Welcome to the Splash App</h1>;
+  return <h1>Welcome to Splash App</h1>;
 }
