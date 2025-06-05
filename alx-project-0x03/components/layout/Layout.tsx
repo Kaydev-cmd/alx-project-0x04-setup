b0f44178e7d822f4e34916d0b0b7ce9f4c8cfba7
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-05 11:23:26 +0200

    feat: create Layout component with Header and Footer imports

---
+++ alx-project-0x03/components/layout/Layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ReactNode } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout: React.FC = () => {
+  return <div>Layout</div>;
+};
+
+export default Layout;
