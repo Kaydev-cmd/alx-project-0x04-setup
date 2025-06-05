4fb515325d7dace95fd5a58bdfa0d4600eaf2d3e
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-05 13:49:06 +0200

    refactor: remove div tags

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -2,9 +2,5 @@ import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to the Splash App</h1>
-    </div>
-  );
+  return <h1>Welcome to the Splash App</h1>;
 }
