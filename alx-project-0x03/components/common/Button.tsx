d6500ed4e5c0597e1dcd8bde29571fc2193f9724
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-05 10:56:25 +0200

    feat: implement dynamic Button component with customizable props

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -1,6 +1,24 @@
+import React from "react";
+
 interface ButtonProps {
-  buttonLabel?: string;
+  buttonLabel: string;
   buttonSize?: string;
   buttonBackgroundColor?: "red" | "blue" | "orange" | "green";
   action?: () => void;
 }
+
+const Button = ({
+  buttonLabel,
+  buttonSize,
+  buttonBackgroundColor,
+  action,
+}: ButtonProps) => {
+  const backgroundColorClass = buttonBackgroundColor
+    ? {
+        red: "bg-red-500",
+        blue: "bg-blue-500",
+        orange: "bg-orange-500",
+        green: "bg-green-500",
+      }[buttonBackgroundColor]
+    : "bg-slate-500";
+};
