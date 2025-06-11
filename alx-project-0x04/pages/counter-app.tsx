357c95f133918e75ed4081b67814151866a04d95
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-11 09:10:48 +0200

    feat: add state and handlers for counter functionality

--- alx-project-0x04/pages/counter-app.tsx
+++ alx-project-0x04/pages/counter-app.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CounterApp: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return <div>CounterApp</div>;
 };
 
