dac703edb2f03960b36c232b351c516b7a30391a
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-11 12:51:23 +0200

    feat: add counter slice with increment and conditional decrement reducers

--- alx-project-0x06/store/store.ts
+++ alx-project-0x06/store/store.ts
@@ -1,2 +1,17 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 import { UseDispatch } from "react-redux";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value > 0 ? (state.value -= 1) : 0;
+    },
+  },
+});
