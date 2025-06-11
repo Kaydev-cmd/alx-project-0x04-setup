20ae517cea4c20ff72eb4570030815d19537bc00
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-11 13:43:32 +0200

    refactor: add return statement

--- alx-project-0x06/store/store.ts
+++ alx-project-0x06/store/store.ts
@@ -27,7 +27,7 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch = () => {
-  useDispatch<AppDispatch>();
+  return useDispatch<AppDispatch>();
 };
 
 export default store;
